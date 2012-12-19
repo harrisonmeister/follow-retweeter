8116e668c072a98569189423f865052c2e472098
Mike Hadlow <mike.hadlow@15below.com>
2012-12-19 10:37:23 +0000

    complete follow retweeter

--- follow-retweeter.js
+++ follow-retweeter.js
@@ -10,8 +10,49 @@ var twit = new twitter({
 	access_token_secret: ''
 });
 
-twit.stream('15below_tech', {track:'nodejs'}, function(stream) {
-	stream.on('data', function(data){
+var hashtag = '#15below'
+var hashtagRegex = new RegExp(hashtag, "i");
+
+function write(data) {
+	if ( typeof data === 'string') {
+		console.log(data);
+	}
+	else if (data.text && data.user && data.user.screen_name) {
+		console.log(data.user.screen_name + ": " + data.text);
+		testForHashtag(data);
+	}
+	else if (data.delete) {
+		console.log('DELETE');
+	}
+	else if (data.message) {
+		console.log('ERROR' + data.message);
+	}
+	else {
 		console.log(util.inspect(data));
+	}
+}
+
+function testForHashtag(data) {
+	if(data.retweeted) return;
+	if(data.text.match(hashtagRegex) !== null) {
+		twit.retweetStatus(data.id_str, function(){
+			console.log('retweet callback');
+		});
+	}
+}
+
+function reconnect() {
+	setTimeout(startStreaming, 1000);
+}
+
+function startStreaming() {
+	twit.stream('user', function(stream) {
+		console.log('starting stream');
+		stream.on('data', write);
+		stream.on('end', reconnect)
 	});
-});
+}
+
+startStreaming();
+
+console.log('lisening for tweets');
